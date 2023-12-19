2e4c2928a267fe9c0ca8f0e2a5252f5abfe4d419
Dhrubajyoti Deb <143205829+dhrubajyotideb@users.noreply.github.com>
2023-12-20 03:54:19 +0530

    third commit

---
+++ reverse String using C/reverseString.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+void reverseString(char* str){
+    int length=strlen(str);
+    int start =0;
+    int end= length-1;
+    while(start<end){
+        char temp=str[start];
+        str[start]=str[end];
+        str[end]=temp;
+        start++;
+        end--;
+    }
+}
+int main(){
+char inputString[100];
+printf("Entrt a String:");
+scanf("%s",&inputString);
+reverseString(inputString);
+printf("Reversed String:%s\n",inputString);
+
+    return 0;
+}
